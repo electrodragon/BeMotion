d651c41283cedfaaa3329fa7942203db07fd2d61
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-03 11:33:59 +0500

    Uploading images

--- includes/explore.php
+++ includes/explore.php
@@ -1,45 +1,34 @@
-<section>
+<section class="explore-section">
     <div class="container">
         <div class="section-title">
             <h2>More Explore</h2>
             <p>Discover our offerings</p>
         </div>
 
-        <div class="explore-grid">
-            <div class="explore-card">
-                <div class="explore-img">01</div>
-                <div class="explore-content">
-                    <h3>Lorem Ipsum</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor.</p>
-                    <a href="#" class="explore-link">Click here <i class="fas fa-arrow-right"></i></a>
+        <div class="explore-layout">
+            <div class="media-block">
+                <div class="video-thumb">
+                    <img src="your-egg-image.jpg" alt="Eggs in Basket" />
+                    <div class="video-btn">▶</div>
+                </div>
+                <div class="text-box">
+                    <h3>Lorem ipsum dolor sit amecon</h3>
+                    <p>Consectetur adipisicing elit, sed do eiusmod tempor</p>
+                    <a href="#" class="explore-btn">More Explore</a>
                 </div>
             </div>
 
-            <div class="explore-card">
-                <div class="explore-img">02</div>
-                <div class="explore-content">
-                    <h3>Lorem Ipsum</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor.</p>
-                    <a href="#" class="explore-link">Click here <i class="fas fa-arrow-right"></i></a>
-                </div>
+            <div class="info-card">
+                <img src="your-chicken-image.jpg" alt="Chickens" />
             </div>
 
-            <div class="explore-card">
-                <div class="explore-img">03</div>
-                <div class="explore-content">
-                    <h3>Lorem Ipsum</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor.</p>
-                    <a href="#" class="explore-link">Click here <i class="fas fa-arrow-right"></i></a>
+            <div class="call-box">
+                <img src="your-avatar.jpg" alt="Avatar" class="avatar" />
+                <div class="call-details">
+                    <span class="call-title">CALL TITLE</span>
+                    <span class="phone-number">PHONE NUMBER</span>
                 </div>
-            </div>
-
-            <div class="explore-card">
-                <div class="explore-img">04</div>
-                <div class="explore-content">
-                    <h3>Lorem Ipsum</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor.</p>
-                    <a href="#" class="explore-link">Click here <i class="fas fa-arrow-right"></i></a>
-                </div>
+                <div class="call-icon">📞</div>
             </div>
         </div>
     </div>
