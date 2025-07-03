d873c681732752966934867bbffa716ff6242279
Attaullah Khan <attaullahkhanlodhi@gmail.com>
2025-07-03 18:38:44 +0500

    heavy

--- includes/our_team_next.php
+++ includes/our_team_next.php
@@ -1,36 +1,7 @@
-<section class="our-team">
+<section class="our-team-next">
 
-    <h1>Our Team</h1>
+    <div class="some-info-frame">
 
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturi,</p>
-
-    <div class="our-team-cards">
-        <div class="our-team-card team-card-1">
-            <img src="/assets/images/team_p_1.jpg" alt="Card image" class="card-image">
-
-            <div class="card-content">
-                <h3 class="card-title">Marc Chiasson</h3>
-                <p class="card-text">Marc Chiasson</p>
-            </div>
-        </div>
-
-        <div class="our-team-card team-card-2">
-            <img src="/assets/images/team_p_2.jpg" alt="Card image" class="card-image">
-
-            <div class="card-content">
-                <h3 class="card-title">Qalim Thams</h3>
-                <p class="card-text">Qalim Thams</p>
-            </div>
-        </div>
-
-        <div class="our-team-card team-card-3">
-            <img src="/assets/images/team_p_3.jpg" alt="Card image" class="card-image">
-
-            <div class="card-content">
-                <h3 class="card-title">Millar Chiasson</h3>
-                <p class="card-text">Millar Chiasson</p>
-            </div>
-        </div>
     </div>
 
 </section>
