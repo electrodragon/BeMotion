d04c7f842dcdfb9dbde529ac8b0a5273e0c9b924
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-03 11:26:46 +0500

    v3

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BeMotion - Automotive Solutions</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
     <?php include('includes/header.php'); ?>
