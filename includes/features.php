b81932d1566d0a25f63d1eac32b4bb0be66d44f6
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-03 11:19:19 +0500

    Set Design of Experience Page

--- includes/features.php
+++ includes/features.php
@@ -16,8 +16,6 @@
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor, aenean massa.
                 Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-                Nulla consequat massa quis enim.
             </p>
             <ul class="features-list">
                 <div class="lists">
