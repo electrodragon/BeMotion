aea26d65eac73c67d9950cb077f76ac28423541b
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-03 13:21:18 +0500

    Set Design of Footer Page

--- includes/footer.php
+++ includes/footer.php
@@ -1,58 +1,72 @@
 <footer>
-    <div class="container">
+    <div class="container content">
+        <div class="footer-top">
+            <div class="footer-call">
+                <div class="call-icon"><i class="fas fa-phone"></i></div>
+                <div class="call-text">
+                    <span class="call-label">Give us a call</span>
+                    <span class="call-number">+123-456-7890</span>
+                </div>
+            </div>
+
+            <hr class="vertical-divider" />
+
+            <div class="footer-newsletter">
+                <h4>Join <br /> Newsletter</h4>
+                <div class="newsletter-form">
+                    <input type="email" placeholder="Your email" />
+                    <button type="submit">Submit</button>
+                </div>
+            </div>
+        </div>
+
         <div class="footer-grid">
             <div class="footer-col">
                 <h3>BeMotion</h3>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor.</p>
                 <div class="social-links">
                     <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#"><i class="fab fa-twitter"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
                     <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="#"><i class="fab fa-skype"></i></a>
                 </div>
             </div>
 
             <div class="footer-col">
                 <h3>Resources</h3>
                 <ul class="footer-links">
-                    <li><a href="#">Lorem Ipsum</a></li>
-                    <li><a href="#">Lorem Ipsum</a></li>
-                    <li><a href="#">Lorem Ipsum</a></li>
-                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Dealership</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
                 </ul>
             </div>
 
             <div class="footer-col">
                 <h3>Company</h3>
                 <ul class="footer-links">
-                    <li><a href="#">Team Services</a></li>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Careers</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
+                    <li><a href="#">Lorem Ipsum</a></li>
                 </ul>
             </div>
 
             <div class="footer-col">
-                <h3>Newsletter</h3>
-                <p>Subscribe to our newsletter for the latest updates.</p>
-
-                <div class="newsletter-form">
-                    <input type="email" placeholder="Your email">
-                    <button type="submit"><i class="fas fa-paper-plane"></i></button>
-                </div>
-
-                <div style="margin-top: 20px; display: flex; align-items: center; gap: 10px;">
-                    <i class="fas fa-phone"></i>
-                    <p>+123-456-7890</p>
-                </div>
+                <h3>Help Page</h3>
+                <ul class="footer-links">
+                    <li><a href="#">Private Policy</a></li>
+                    <li><a href="#">Team services</a></li>
+                </ul>
             </div>
         </div>
     </div>
 
+<!--    <hr />-->
+
     <div class="copyright">
         <div class="container">
-            <p>&copy; 2023 BeMotion. All Rights Reserved. Designed with <i class="fas fa-heart" style="color: var(--secondary);"></i></p>
+            <p>&copy; Copyright 2023 All rights reserved.</p>
         </div>
     </div>
 </footer>
