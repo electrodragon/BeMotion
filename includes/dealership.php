fd9b7474d541714af93304e1bb5c9142f0000862
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-04 23:56:17 +0500

    Add Trainsition on COmapany Page

--- includes/dealership.php
+++ includes/dealership.php
@@ -2,47 +2,47 @@
     <div class="container">
 
         <div class="elements-group element-group-one">
-            <h3>Lorem Ipsum</h3>
-            <p>Lorem ipsum dolor sit amecon<br />
+            <h3 data-aos="fade-down" data-aos-delay="450" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">Lorem Ipsum</h3>
+            <p data-aos="fade-down" data-aos-delay="250" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">Lorem ipsum dolor sit amecon<br />
                 sectetur adipisicing elit, sed do<br />
                 eiusmod tempor</p>
-            <div class="image-container">
+            <div class="image-container" data-aos="fade-down" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-cubic" >
                 <img src="/assets/images/sec_4_first_icon.png" alt="icon" />
             </div>
-            <h2>01</h2>
+            <h2 data-aos="zoom-in" data-aos-delay="650" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">01</h2>
         </div>
 
         <div class="elements-group element-group-two">
-            <h3>Lorem Ipsum</h3>
-            <p>Lorem ipsum dolor sit amecon<br />
+            <h3 data-aos="fade-up" data-aos-delay="450" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">Lorem Ipsum</h3>
+            <p data-aos="fade-up" data-aos-delay="250" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">Lorem ipsum dolor sit amecon<br />
                 sectetur adipisicing elit, sed do<br />
                 eiusmod tempor</p>
-            <div class="image-container">
+            <div class="image-container" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-cubic" >
                 <img src="/assets/images/sec_4_second_icon.png" alt="icon" />
             </div>
-            <h2>02</h2>
+            <h2 data-aos="zoom-in" data-aos-delay="650" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">02</h2>
         </div>
 
         <div class="elements-group element-group-three">
-            <h3>Lorem Ipsum</h3>
-            <p>Lorem ipsum dolor sit amecon<br />
+            <h3 data-aos="fade-down" data-aos-delay="450" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">Lorem Ipsum</h3>
+            <p data-aos="fade-down" data-aos-delay="250" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">Lorem ipsum dolor sit amecon<br />
                 sectetur adipisicing elit, sed do<br />
                 eiusmod tempor</p>
-            <div class="image-container">
+            <div class="image-container" data-aos="fade-down" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-cubic">
                 <img src="/assets/images/sec_4_third_icon.png" alt="icon" />
             </div>
-            <h2>03</h2>
+            <h2 data-aos="zoom-in" data-aos-delay="650" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">03</h2>
         </div>
 
         <div class="elements-group element-group-four">
-            <h3>Lorem Ipsum</h3>
-            <p>Lorem ipsum dolor sit amecon<br />
+            <h3 data-aos="fade-up" data-aos-delay="450" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">Lorem Ipsum</h3>
+            <p data-aos="fade-up" data-aos-delay="250" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">Lorem ipsum dolor sit amecon<br />
                 sectetur adipisicing elit, sed do<br />
                 eiusmod tempor</p>
-            <div class="image-container">
+            <div class="image-container" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-cubic" >
                 <img src="/assets/images/sec_4_fifth_icon.png" alt="icon" />
             </div>
-            <h2>04</h2>
+            <h2 data-aos="zoom-in" data-aos-delay="650" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">04</h2>
         </div>
 
 
