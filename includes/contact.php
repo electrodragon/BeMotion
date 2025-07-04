f03c24557dfa4e83732559181c635cba631d2926
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-05 00:55:16 +0500

    Add transition on contact_us page

--- includes/contact.php
+++ includes/contact.php
@@ -1,31 +1,31 @@
 <section>
     <div class="container">
-        <div class="title">
+        <div class="title" data-aos="zoom-in" data-aos-duration="2000" data-aos-offset="200" data-aos-easing="ease">
             <h2>CONTACT US</h2>
         </div>
 
         <form class="contact-form">
-            <div class="form-group">
+            <div class="form-group" data-aos="fade-right" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
                 <input type="text" placeholder="First name">
             </div>
 
-            <div class="form-group">
+            <div class="form-group" data-aos="fade-left" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
                 <input type="text" placeholder="Last name">
             </div>
 
-            <div class="form-group">
+            <div class="form-group" data-aos="fade-right" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
                 <input type="tel" placeholder="Phone number">
             </div>
 
-            <div class="form-group">
+            <div class="form-group" data-aos="fade-left" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
                 <input type="text" placeholder="Location (City)">
             </div>
 
-            <div class="form-group">
+            <div class="form-group" data-aos="fade-right" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
                 <input type="email" placeholder="Email address">
             </div>
 
-            <div class="form-group">
+            <div class="form-group" data-aos="fade-left" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
                 <select>
                     <option disabled selected>Position</option>
                     <option>Dealership Inquiry</option>
@@ -34,19 +34,19 @@
                 </select>
             </div>
 
-            <div class="form-group">
+            <div class="form-group" data-aos="fade-right" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
                 <input type="text" placeholder="Portfolio / Website link..">
             </div>
 
-            <div class="form-group">
+            <div class="form-group" data-aos="fade-left" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
                 <input type="text" placeholder="Your expected salary">
             </div>
 
-            <div class="form-group form-full">
+            <div class="form-group form-full" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
                 <textarea placeholder="Write your message..." rows="5"></textarea>
             </div>
 
-            <div class="form-group form-full btn-area center">
+            <div class="form-group form-full btn-area center" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="50" data-aos-easing="ease-in-out">
                 <button type="submit" class="btn-submit">SUBMIT NOW</button>
             </div>
         </form>
