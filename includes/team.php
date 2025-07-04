82748b541093c25f270b2927e789b52e5bb68b0f
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-05 00:35:57 +0500

    Add transition on team page

--- includes/team.php
+++ includes/team.php
@@ -3,34 +3,34 @@
     <div class="team-highlight-container">
         <div class="highlight-layout">
 
-            <div class="highlight-left">
+            <div class="highlight-left" data-aos="zoom-in" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
                 <img src="/assets/images/sprayer.png" alt="Worker Spraying" class="highlight-img">
             </div>
 
             <div class="highlight-right">
-                <p class="highlight-desc">
+                <p class="highlight-desc" data-aos="fade-down" data-aos-delay="50" data-aos-duration="2000" data-aos-offset="200" data-aos-easing="ease" >
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
                 </p>
 
                 <div class="highlights">
-                    <div class="highlight-stats">
+                    <div data-aos="fade-right" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out" class="highlight-stats">
                         <h2 class="stat-number">9625</h2>
                         <p class="stat-label">Started Journey</p>
                     </div>
 
-                    <ul class="highlight-benefits">
+                    <ul data-aos="fade-left" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out" class="highlight-benefits">
                         <li><i class="fas fa-check-circle"></i> Early Engagement</li>
                         <li><i class="fas fa-check-circle"></i> Affordable Price upto 2 years</li>
                     </ul>
                 </div>
 
-                <a href="#" class="highlight-btn">More Explore</a>
+                <a href="#" class="highlight-btn" data-aos="fade-right" data-aos-duration="2000" data-aos-easing="ease-in-out">More Explore</a>
             </div>
         </div>
     </div>
 
     <!-- Right Worker Image -->
-    <div class="highlight-character">
+    <div data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease-in-out" class="highlight-character">
         <img src="../assets/images/person.png" alt="Smiling Worker" class="character-img">
     </div>
 
