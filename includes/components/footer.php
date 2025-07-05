624ac5e3b578225e2803389a8e837a08c3e641e6
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-05 16:37:50 +0500

    Footer ki responsiveness add kr raha complete ni hui

--- includes/components/footer.php
+++ includes/components/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container content">
-        <div class="footer-top">
-            <div class="footer-call">
+        <div class="footer-top row gx-4 gy-4 align-items-center">
+            <div class="footer-call col-12 col-md-5 d-flex align-items-center gap-3">
                 <div class="call-icon"><i class="fas fa-phone"></i></div>
                 <div class="call-text">
                     <span class="call-label">Give us a call</span>
@@ -9,19 +9,21 @@
                 </div>
             </div>
 
-            <hr class="vertical-divider" />
+            <div class="d-none d-md-block col-md-1 text-center">
+                <hr class="vertical-divider mx-auto" />
+            </div>
 
-            <div class="footer-newsletter">
-                <h4>Join <br /> Newsletter</h4>
-                <div class="newsletter-form">
-                    <input type="email" placeholder="Your email" />
+            <div class="footer-newsletter col-12 col-md-6 text-md-end text-center">
+                <h4 class="mb-2 text-white text-start">Join <br /> Newsletter</h4>
+                <div class="newsletter-form d-inline-flex flex-wrap justify-content-center justify-content-md-end">
+                    <input type="email" placeholder="Your email" class="mb-2 mb-md-0" />
                     <button type="submit">Submit</button>
                 </div>
             </div>
         </div>
 
-        <div class="footer-grid">
-            <div class="footer-col">
+        <div class="footer-grid row py-4 gx-4 gy-5">
+            <div class="footer-col col-12 col-md-6 col-lg-3">
                 <h3>BeMotion</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor.</p>
                 <div class="social-links">
@@ -32,7 +34,7 @@
                 </div>
             </div>
 
-            <div class="footer-col">
+            <div class="footer-col col-6 col-md-6 col-lg-3">
                 <h3>Resources</h3>
                 <ul class="footer-links">
                     <li><a href="#">About Us</a></li>
@@ -43,7 +45,7 @@
                 </ul>
             </div>
 
-            <div class="footer-col">
+            <div class="footer-col col-6 col-md-6 col-lg-3">
                 <h3>Company</h3>
                 <ul class="footer-links">
                     <li><a href="#">Lorem Ipsum</a></li>
@@ -52,17 +54,15 @@
                 </ul>
             </div>
 
-            <div class="footer-col">
+            <div class="footer-col col-12 col-md-6 col-lg-3">
                 <h3>Help Page</h3>
                 <ul class="footer-links">
                     <li><a href="#">Private Policy</a></li>
-                    <li><a href="#">Team services</a></li>
+                    <li><a href="#">Team Services</a></li>
                 </ul>
             </div>
         </div>
     </div>
-
-<!--    <hr />-->
 
     <div class="copyright">
         <div class="container">
