3097a5a3249025460e30344584d17a8b7b8d69df
Attaullah Khan <attaullahkhanlodhi@gmail.com>
2025-07-05 17:16:59 +0500

    hello about

--- includes/pages/blogs/blog_categories_section.php
+++ includes/pages/blogs/blog_categories_section.php
@@ -1,27 +1,52 @@
-<section class="blog-categories-section">
+<section class="blog-categories-section pt-5">
     <div class="categories-content-area">
+        <div class="container">
 
-        <div class="some-logo">
-            <img src="/assets/images/blogs_page/logoless_box.png" alt="some logo">
-        </div>
-
-        <div class="text-group">
-            <h1>Lorem ipsum dolor sit amet, congfs</h1>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-        </div>
-
-        <div class="categories-bar-area">
-            <div class="categories-bar-left-area">
-                <span class="category">ALL</span>
-                <span class="category">dladhlad</span>
-                <span class="category">alsdhlaad</span>
-                <span class="category">adnamdp adjad</span>
-                <i class="fa-solid fa-magnifying-glass"></i>
+            <div class="some-logo">
+                <img src="/assets/images/blogs_page/logoless_box.png" alt="some logo">
             </div>
-            <div class="categories-bar-right-area">
 
+            <div class="row mt-3">
+                <div class="col-12">
+                    <h1 class="text-center">Lorem ipsum dolor sit amet, congfs</h1>
+                </div>
             </div>
-        </div>
 
+            <div class="row justify-content-center mb-2">
+                <div class="col-md-8">
+                    <p class="text-center">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Lorem ipsum dolor sit amet,
+                        consectetuer adipiscing elit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="categories-bar py-2 d-flex justify-content-between align-items-center flex-wrap">
+
+                        <div class="categories-left">
+                            <span class="category">ALL</span>
+                            <span class="category">dladhlad</span>
+                            <span class="category">alsdhlaad</span>
+                            <span class="category">adnamdp adjad</span>
+                            <i class="fa-solid fa-magnifying-glass ms-2"></i>
+                        </div>
+
+                        <div class="categories-right">
+                            <div class="author-image">
+                                <img src="/assets/images/blogs_page/gabie.png" alt="author">
+                            </div>
+                            <div class="author-detail">
+                                <small class="d-block text-muted">BLOG AUTHOR</small>
+                                <span class="author-name fw-semibold">Gabie Sheber</span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 </section>
