c353aef908a202a9d4f2cadff1b4bed0daed4dc6
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-06 17:02:03 +0500

    About Page done 2 section responsiveness rehti

--- about.php
+++ about.php
@@ -7,6 +7,7 @@
     <?php include('includes/pages/about/what_we_do.php'); ?>
     <?php include('includes/pages/about/construction_solution.php'); ?>
     <?php include('includes/pages/about/our_team_display.php'); ?>
+    <?php include('includes/pages/about/about_blogs.php'); ?>
 
     <?php include('includes/components/footer.php'); // Component ?>
 
---
+++ includes/pages/about/about_blogs.php
@@ -0,0 +1,76 @@
+<section class="about-blogs">
+
+    <div class="container px-5">
+
+        <div class="blogs-container">
+            <div class="bog-title" data-aos="zoom-in" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
+                <h1>Blogs</h1>
+            </div>
+
+            <div class="blog-content">
+                <div class="col-sm-6 px-4 mt-5">
+                    <div class="blog-card">
+                        <div class="card-image-container" data-aos="fade-right" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
+                            <img src="/assets/images/blogs_page/loving-hens.jpg" alt="granny" class="rounded">
+                        </div>
+
+                        <h3 class="mt-4" data-aos="fade-in" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h3>
+
+                        <p class="mt-4" data-aos="fade-down" data-aos-duration="1000" data-aos-easing="ease-in-out">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Lorem ipsum dolor sit amet,
+                            consectetuer adipiscing elit.</p>
+
+                        <div class="chips d-flex gap-3 mt-4">
+                            <span class="chip">Lorem</span>
+                            <span class="chip">zdfsfloreamsfsrfsf</span>
+                            <span class="chip">
+                                <img src="/assets/images/blogs_page/Boston%20Celtics.svg" alt="boston">
+                                                                      India
+                            </span>
+                        </div>
+
+                        <div class="d-flex align-items-center gap-5 mt-3">
+                            <span class="blog-author-name" data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease-in-out">By Gabie Shebber</span>
+                            <span class="blog-publish-date" data-aos="fade-right" data-aos-delay="100" data-aos-duration="1000" data-aos-easing="ease-in-out">
+                            <span class="blog-publish-date-dot" data-aos="fade-right" data-aos-delay="150" data-aos-duration="1000" data-aos-easing="ease-in-out"></span>
+                                      May 22, 2020
+                            </span>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-sm-6 px-4 mt-5">
+                    <div class="blog-card">
+                        <div class="card-image-container" data-aos="fade-left" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
+                            <img src="/assets/images/blogs_page/brown_chicken_farms.jpg" alt="granny" class="rounded">
+                        </div>
+
+                        <h3 class="mt-4" data-aos="fade-in" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h3>
+
+                        <p class="mt-4" data-aos="fade-down" data-aos-duration="1000" data-aos-easing="ease-in-out">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Lorem ipsum dolor sit amet,
+                            consectetuer adipiscing elit.</p>
+
+                        <div class="chips d-flex gap-3 mt-4">
+                            <span class="chip">Lorem</span>
+                            <span class="chip">zdfsfloreamsfsrfsf</span>
+                            <span class="chip">
+<img src="/assets/images/blogs_page/Boston%20Celtics.svg" alt="boston">
+                                                                      India
+</span>
+                        </div>
+
+                        <div class="d-flex align-items-center gap-5 mt-3">
+                            <span class="blog-author-name" data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease-in-out">By Gabie Shebber</span>
+                            <span class="blog-publish-date" data-aos="fade-left" data-aos-delay="100" data-aos-duration="1000" data-aos-easing="ease-in-out">
+                            <span class="blog-publish-date-dot" data-aos="fade-left" data-aos-delay="150" data-aos-duration="1000" data-aos-easing="ease-in-out"></span>
+                                      May 22, 2020
+                            </span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
