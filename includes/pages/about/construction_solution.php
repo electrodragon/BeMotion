763f9ece9d8c9a56b300c232d75397ed8eea5d15
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-06 14:35:54 +0500

    Design and responsive of construction-solution page

---
+++ includes/pages/about/construction_solution.php
@@ -0,0 +1,4 @@
+<div class="cta-banner d-flex flex-wrap align-items-center justify-content-between my-4">
+    <span class="cta-text">Offering High Quality Construction Solutions</span>
+    <a href="#" class="btn-cta">Build Your Dream Now</a>
+</div>
