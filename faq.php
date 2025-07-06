5e2420d345aac68075fa60930dd744441c02aed4
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-06 21:04:14 +0500

    Complete design & responsiveness of Question part in FAQ page

--- faq.php
+++ faq.php
@@ -3,17 +3,7 @@
     <?php include('includes/components/header.php'); // Component ?>
 
     <?php include('includes/components/page_identifier.php'); pageIdentifier('FAQ\'S', 'FAQ'); ?>
-    <?php include('includes/pages/faq/hero.php'); ?>
-    <?php include('includes/pages/faq/features.php'); ?>
-    <?php include('includes/pages/faq/explore.php'); ?>
-    <?php include('includes/pages/faq/dealership.php'); ?>
-    <?php include('includes/pages/faq/company.php'); ?>
-    <?php include('includes/pages/faq/get_dealership.php'); ?>
-    <?php include('includes/pages/faq/our_team.php'); ?>
-    <?php include('includes/pages/faq/our_team_next.php'); ?>
-    <?php include('includes/pages/faq/team.php'); ?>
-    <?php include('includes/pages/faq/benefits.php'); ?>
-    <?php include('includes/pages/faq/contact.php'); ?>
+    <?php include('includes/pages/faq/questions.php'); ?>
 
     <?php include('includes/components/footer.php'); // Component ?>
 
---
+++ includes/pages/faq/questions.php
@@ -0,0 +1,130 @@
+<section class="faq-section">
+    <div class="container-fluid px-5">
+        <div class="faq-wrapper d-flex flex-wrap justify-content-between align-items-start gap-5">
+
+            <!-- top Side -->
+            <div class="faq-top">
+                <div class="titles">
+
+                    <div class="sub-title" data-aos="zoom-in" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out" >
+                        <div class="img">
+                            <img src="/assets/images/product_page/sc_icon.png" alt="sc_icon">
+                        </div>
+                        <p class="faq-subtitle text-warning">   Great Experience in building</p>
+                    </div>
+
+                    <h2 class="faq-heading" data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease-in-out">Frequently Asked<br><span>Any Questions</span></h2>
+                </div>
+
+                <p class="faq-desc" data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease-in-out">
+                    Aliquam tempus libero eget arcu euismod, in bibendum nisl posuere. Donec gravida sem eu dolor rhoncus viverra, in vel cursus ante.
+                </p>
+            </div>
+
+
+<!--             bottom Side - FAQs -->
+            <div class="faq-bottom">
+<!--                Images-->
+                <div class="faq-images mt-4">
+                    <img src="/assets/images/faqs_page/cow_bulls_cowshed_farm.jpg" class="faq-main-img rounded-5 mb-3" alt="hens" data-aos="zoom-in" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
+                    <img src="/assets/images/faqs_page/worker.png" class="faq-sub-img" alt="workers" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
+                </div>
+
+                <div class="accordion" id="faqAccordion">
+                    <div class="accordion-item" data-aos="fade-down" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#q1">
+                                Lorem ispueam
+                            </button>
+                        </h2>
+                        <div id="q1" class="accordion-collapse collapse show" data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                Improve efficiency, provide a better customer experience with modern technolo services available around Improve efficiency, provide a better customer experience
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#q2">
+                                Lorem ispum akiqab temous iber egger euimid
+                            </button>
+                        </h2>
+                        <div id="q2" class="accordion-collapse collapse" data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                Improve efficiency, provide a better customer experience with modern technology services available.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item" data-aos="fade-down" data-aos-delay="200" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#q3">
+                                Lorem ispum akiqab temous iber egger euimid
+                            </button>
+                        </h2>
+                        <div id="q3" class="accordion-collapse collapse" data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                Improve efficiency, provide a better customer experience with modern technology services available.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item" data-aos="fade-down" data-aos-delay="300" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#q4">
+                                Lorem ispum akiqab temous iber egger euimid
+                            </button>
+                        </h2>
+                        <div id="q4" class="accordion-collapse collapse" data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                Improve efficiency, provide a better customer experience with modern technology services available.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item" data-aos="fade-down" data-aos-delay="400" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#q5">
+                                Lorem ispum akiqab temous iber egger euimid
+                            </button>
+                        </h2>
+                        <div id="q5" class="accordion-collapse collapse" data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                Improve efficiency, provide a better customer experience with modern technology services available.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item" data-aos="fade-down" data-aos-delay="500" data-aos-duration="1000" data-aos-easing="ease-in-out">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#q6">
+                                Lorem ispum akiqab temous iber egger euimid
+                            </button>
+                        </h2>
+                        <div id="q6" class="accordion-collapse collapse" data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                Improve efficiency, provide a better customer experience with modern technology services available.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item" data-aos="fade-down" data-aos-delay="600" data-aos-duration="1000" data-aos-easing="ease-in-out">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#q7">
+                                Lorem ispum akiqab temous iber egger euimid
+                            </button>
+                        </h2>
+                        <div id="q7" class="accordion-collapse collapse" data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                Improve efficiency, provide a better customer experience with modern technology services available.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</section>
