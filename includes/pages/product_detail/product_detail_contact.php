73416b1423f05dd6968b3b60611a2eeed9fb9b48
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-07 09:02:08 +0500

    Complete design and responsiveness of contact me page in product detail page

---
+++ includes/pages/product_detail/product_detail_contact.php
@@ -0,0 +1,37 @@
+<div class="form-container">
+    <div class="titles">
+        <h2>Ipsam voluptatem voluptas </h2>
+        <p>Donec purus mi, commodo id commodo vel, imperdiet ut mauris. Ut ultricies arcu risus, malesuada efficitur orci euismod in. Proin eleifend est risus, ac
+            sodales nulla mollis vel.</p>
+    </div>
+
+    <div class="form-wrapper" data-aos="zoom-in" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
+        <h3 class="form-title">Contact Me</h3>
+        <p class="form-subtitle">In nec libero luctus, aliquet turpis at, vehicula nisi. Cras eget mauris in nisl tempus lobortis.</p>
+
+        <form class="styled-form">
+            <div class="form-group">
+                <input type="text" placeholder="Fast name" required />
+                <input type="tel" placeholder="Phone number" required />
+            </div>
+
+            <div class="form-group">
+                <input type="email" placeholder="Email address" required />
+                <select required>
+                    <option disabled selected>Select Service Type</option>
+                    <option>Technical Support</option>
+                    <option>Partnership</option>
+                    <option>Consulting</option>
+                </select>
+            </div>
+
+            <div class="form-group full">
+                <textarea rows="5" placeholder="Type your message" required></textarea>
+            </div>
+
+            <div class="form-group full center">
+                <button type="submit" class="btn-submit">SEND MESSAGE</button>
+            </div>
+        </form>
+    </div>
+</div>
