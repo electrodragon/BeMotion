94757c16b8d61a16991277d5f492f9c85584044f
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-08 20:07:47 +0500

    hero section responsiveness done

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
     <?php include('includes/components/header.php'); // Component ?>
 
     <?php include('includes/pages/home/hero.php'); ?>
-    <?php include('includes/pages/home/features.php'); ?>
-    <?php include('includes/pages/home/explore.php'); ?>
-    <?php include('includes/pages/home/dealership.php'); ?>
-    <?php include('includes/pages/home/company.php'); ?>
-    <?php include('includes/pages/home/get_dealership.php'); ?>
-    <?php include('includes/pages/home/our_team.php'); ?>
-    <?php include('includes/pages/home/our_team_next.php'); ?>
-    <?php include('includes/pages/home/team.php'); ?>
-    <?php include('includes/pages/home/benefits.php'); ?>
-    <?php include('includes/pages/home/contact.php'); ?>
+<!--    --><?php //include('includes/pages/home/features.php'); ?>
+<!--    --><?php //include('includes/pages/home/explore.php'); ?>
+<!--    --><?php //include('includes/pages/home/dealership.php'); ?>
+<!--    --><?php //include('includes/pages/home/company.php'); ?>
+<!--    --><?php //include('includes/pages/home/get_dealership.php'); ?>
+<!--    --><?php //include('includes/pages/home/our_team.php'); ?>
+<!--    --><?php //include('includes/pages/home/our_team_next.php'); ?>
+<!--    --><?php //include('includes/pages/home/team.php'); ?>
+<!--    --><?php //include('includes/pages/home/benefits.php'); ?>
+<!--    --><?php //include('includes/pages/home/contact.php'); ?>
 
-    <?php include('includes/components/footer.php'); // Component ?>
+<!--    --><?php //include('includes/components/footer.php'); // Component ?>
 
 <?php include('includes/partials/footer.php'); // Partials ?>
