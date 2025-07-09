dbe1bbba191f562aa54662b1e0042f4bee28b751
Attaullah Khan <attaullahkhanlodhi@gmail.com>
2025-07-09 13:22:26 +0500

    youtube

--- includes/pages/home/explore.php
+++ includes/pages/home/explore.php
@@ -1,54 +1,57 @@
 <section class="explore-section">
     <div class="explore-layout">
 
-        <div class="explore-layout-left" data-aos="fade-left" data-aos-delay="150" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
-            <div class="overlay-content">
+        <iframe src="https://www.youtube.com/embed/SddMq2nKsUA?si=oDVjfQG1Wl60Cz78" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
-                <div class="image-content">
-                    <div class="img">
-                        <img src="../assets/images/eggs.png" alt="eggs-basket">
-                    </div>
-                </div>
-
-                <div class="media-button-box">
-                    <img src="/assets/images/watch_video_text.png" alt="png">
-                    <span class="video-btn">▶</span>
-                </div>
-
-
-                <div class="cell-box-container">
-                    <div class="call-box">
-                        <img src="/assets/images/worker.png" alt="Avatar" class="avatar"/>
-                        <div class="call-icon">📞</div>
-                        <div class="call-details">
-                            <span class="call-title">CALL TITLE</span>
-                            <span class="phone-number">PHONE NUMBER</span>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-        <div class="explore-layout-right" data-aos="fade-right" data-aos-delay="150" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">
-            <div class="overlay-content">
-                <div class="image-content">
-                    <div class="img">
-                        <img src="../assets/images/hens.png" alt="eggs-basket">
-                    </div>
-                </div>
-
-                <div class="lorem-text">
-                    Lorem ipsum dolor sit amecon<br />
-                    sectetur adipisicing elit, sed do<br />
-                    eiusmod tempor
-                </div>
-
-                <div class="button-area">
-                    <button>More Explore</button>
-                </div>
-            </div>
-        </div>
+<!---->
+<!--        <div class="explore-layout-left" data-aos="fade-left" data-aos-delay="150" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">-->
+<!--            <div class="overlay-content">-->
+<!---->
+<!--                <div class="image-content">-->
+<!--                    <div class="img">-->
+<!--                        <img src="../assets/images/eggs.png" alt="eggs-basket">-->
+<!--                    </div>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="media-button-box">-->
+<!--                    <img src="/assets/images/watch_video_text.png" alt="png">-->
+<!--                    <span class="video-btn">▶</span>-->
+<!--                </div>-->
+<!---->
+<!---->
+<!--                <div class="cell-box-container">-->
+<!--                    <div class="call-box">-->
+<!--                        <img src="/assets/images/worker.png" alt="Avatar" class="avatar"/>-->
+<!--                        <div class="call-icon">📞</div>-->
+<!--                        <div class="call-details">-->
+<!--                            <span class="call-title">CALL TITLE</span>-->
+<!--                            <span class="phone-number">PHONE NUMBER</span>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!---->
+<!--            </div>-->
+<!--        </div>-->
+<!---->
+<!--        <div class="explore-layout-right" data-aos="fade-right" data-aos-delay="150" data-aos-duration="1000" data-aos-offset="200" data-aos-easing="ease-in-out">-->
+<!--            <div class="overlay-content">-->
+<!--                <div class="image-content">-->
+<!--                    <div class="img">-->
+<!--                        <img src="../assets/images/hens.png" alt="eggs-basket">-->
+<!--                    </div>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="lorem-text">-->
+<!--                    Lorem ipsum dolor sit amecon<br />-->
+<!--                    sectetur adipisicing elit, sed do<br />-->
+<!--                    eiusmod tempor-->
+<!--                </div>-->
+<!---->
+<!--                <div class="button-area">-->
+<!--                    <button>More Explore</button>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
 
     </div>
 </section>
