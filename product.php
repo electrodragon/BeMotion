13776bcf19ac614dc4de619333b34b0cec8288e3
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2025-07-14 15:50:49 +0500

    NO cache

--- product.php
+++ product.php
@@ -1,4 +1,11 @@
-<?php header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");?>
+<?php
+// Force cache to clear every time
+header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
 <?php include('includes/partials/header.php'); page_header('BeMotion - Product');// Partials ?>
 
     <?php include('includes/components/header.php'); // Component ?>
